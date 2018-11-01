60b2df11a10b639b92490103653f2baf10e74c1c
CJ <cjbrice22@gmail.com>
2018-11-01 10:41:43 -0700

    11/1/18
    Added jumping, animations, bug fixes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,19 +7,33 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    Rigidbody2D myRigidBody;
-    [SerializeField] float runSpeed = 5f;
 
-    // Use this for initialization
+    // Config
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+
+    // State
+    bool isAlive = true;
+    bool IsTouchingLayers(LayerMask.GetMask("Ground"))
+
+    // Cached component references
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    Collider2D myCollider2D;
+
+    // Messages then methods
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        Jump();
         FlipSprite();
     }
 
@@ -29,6 +43,20 @@ public class Player : MonoBehaviour
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void Jump()
+    {
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2 (0, jumpSpeed);
+            myRigidBody.velocity = jumpVelocityToAdd;
+        }
     }
 
     private void FlipSprite()
